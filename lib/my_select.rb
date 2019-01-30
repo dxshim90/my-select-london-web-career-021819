c1a7fa4ec5bd874bab004dd19f3af347dffbc2ff
Daniel Shimield <dxshim90@gmail.com>
2019-01-30 16:56:20 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
  newarr = []
  i = 0 
  while i < collection.length 
- if collection[i] == true
- newarr << yield(collection[i])
+ if yield collection[i] == true
+ newarr << collection[i]
  end
  newarr
  end
